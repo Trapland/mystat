4515f25df37d10bcad7c708f7dc85680968b3206
Trapland <89914789+Trapland@users.noreply.github.com>
2022-09-27 21:17:23 +0200

    Добавьте файлы проекта.

---
+++ mystat/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mystat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            Green1.IsChecked = true;
+            Green2.IsChecked = true;
+            Green3.IsChecked = true;
+            Green4.IsChecked = true;
+            Green5.IsChecked = true;
+            Green6.IsChecked = true;
+        }
+    }
+}
